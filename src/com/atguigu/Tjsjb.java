61333397ff95f55a3bfe487cf84d3a1d89a5f88d
zwj <mmrxxoo@126.com>
2016-12-11 14:21:20 +0800

    更新1

--- src/com/atguigu/Tjsjb.java
+++ src/com/atguigu/Tjsjb.java
@@ -5,6 +5,7 @@ public class Tjsjb {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("wwwwwwwwwwww");
+		System.out.println("qqqqqqqqqqqqq");
 	}
 
 }
